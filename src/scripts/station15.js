f9d34f1431831cf12785fd0dde74e9406e91fb79
gata <hayato.yamagata@icloud.com>
2024-03-13 23:00:56 +0900

    finish_Railway

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,4 +1,5 @@
 async function getData() {
+  
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
@@ -11,6 +12,18 @@ async function getData() {
 }
 
 function test() {
-  return
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const userList = [
+        { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
+        { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+      ];
+      const fullNames = userList.map(user => ({
+        ...user,
+        full_name: `${user.family_name} ${user.first_name}`
+      }));
+      resolve(fullNames);  
+    }, 3000);  
+  });
 }
 
